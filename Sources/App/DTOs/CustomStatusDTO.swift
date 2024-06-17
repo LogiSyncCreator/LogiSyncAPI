eb15322416d56aafd6319cc6cc65cfcd5ec6ff5b
me990928 <me990928@gmail.com>
2024-06-18 01:12:22 +0900

    #2 カスタムステータス

--- Sources/App/DTOs/CustomStatusDTO.swift
+++ Sources/App/DTOs/CustomStatusDTO.swift
@@ -4,5 +4,31 @@
 //
 //  Created by 広瀬友哉 on 2024/06/18.
 //
+import Fluent
+import Vapor
 
-import Foundation
+struct CustomStatusDTO: Content {
+    var id: UUID?
+    var manager: String?
+    var shipper: String?
+    var name: String?
+    var delete: Bool?
+    
+    func toModel() -> CustomStatus {
+        let model = CustomStatus()
+        
+        model.id = self.id
+        if let manager = self.manager,
+           let shipper = self.shipper,
+           let name = self.name,
+           let delete = self.delete {
+            model.manager = manager
+            model.shipper = shipper
+            model.name = name
+            model.delete = delete
+        }
+        return model
+    }
+}
+
+
--- Sources/App/Migrations/CreateCustomStatus.swift
+++ Sources/App/Migrations/CreateCustomStatus.swift
@@ -5,4 +5,20 @@
 //  Created by 広瀬友哉 on 2024/06/18.
 //
 
-import Foundation
+import Fluent
+
+struct CreateCustomStatus: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        try await database.schema("custom_status")
+            .id()
+            .field("manager_id", .string, .required)
+            .field("shipper_id", .string, .required)
+            .field("name", .string, .required)
+            .field("delete", .bool, .required)
+            .create()
+    }
+
+    func revert(on database: Database) async throws {
+        try await database.schema("custom_status").delete()
+    }
+}
