0e28c145cc29b87794c64f2d473cc2f95ada1361
me990928 <me990928@gmail.com>
2024-06-18 12:21:58 +0900

    #4 マッチングテーブル

---
+++ Sources/App/DTOs/MatchingDTO.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  
+//
+//  Created by 広瀬友哉 on 2024/06/18.
+//
+
+import Foundation
+import Fluent
+import Vapor
+
+struct MatchingDTO: Content {
+    var id: UUID?
+    var manager: String?
+    var shipper: String?
+    var driver: String?
+    var start: Date?
+    var address: String?
+    var delete: Bool?
+    
+    func toModel() -> Matching {
+        let model = Matching()
+        
+        model.id = self.id
+        model.start = self.start
+        if let manager = self.manager,
+           let shipper = self.shipper,
+           let driver = self.driver,
+           let address = self.address,
+           let delete = self.delete {
+            model.manager = manager
+            model.shipper = shipper
+            model.driver = driver
+            model.address = address
+            model.delete = delete
+        }
+        
+        return model
+    }
+}
