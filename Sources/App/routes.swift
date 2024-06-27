1d94f6ba4922b8d0d79ebb89cdf3afc223036b96
me990928 <me990928@gmail.com>
2024-06-27 23:31:13 +0900

    プッシュ通知エンドポイント追加

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -10,40 +10,6 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
-    
-    
-    app.get("test-push") { req async throws -> HTTPStatus in
-        
-        let payload = Payload(title: "test", body: "push", like: "cc")
-        
-        let alert = APNSAlertNotification(
-            alert: .init(
-                title: .raw("LogiSync"),
-                body: .raw("テスト通知")
-            ),
-            expiration: .immediately,
-            priority: .immediately,
-            topic: "com.nanaSoft.LogiSync",
-            payload: payload,
-            sound: .default
-        )
-        
-        try await req.apns.client.sendAlertNotification(alert, deviceToken: "3b7cc924c3ed8934bc87604a43f5fa99f2c52d9fa0f5cf8416f69febe28ca774")
-        
-        return .ok
-    }
-    
-    app.get("silent") { req async throws -> HTTPStatus in
-        
-        let payload = Payload(title: "silent", body: "background", like: "hi")
-        
-        let alert = APNSBackgroundNotification(expiration: .immediately, topic:  "com.nanaSoft.LogiSync", payload: payload)
-        
-        try await req.apns.client.sendBackgroundNotification(alert, deviceToken: "3b7cc924c3ed8934bc87604a43f5fa99f2c52d9fa0f5cf8416f69febe28ca774")
-        
-        return .ok
-        
-    }
 
     try app.register(collection: TodoController())
     try app.register(collection: AccountController())
@@ -52,6 +18,7 @@ func routes(_ app: Application) throws {
     try app.register(collection: MatchingController())
     try app.register(collection: DeviceTokenController())
     try app.register(collection: LocationController())
+    try app.register(collection: PushController())
 }
 
 struct Payload: Codable {
